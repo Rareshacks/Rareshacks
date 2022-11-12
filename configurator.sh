35910a79fe2e85c4af5f8083d3fcbedbf17b10b7
Rareshacks <36563630+Rareshacks@users.noreply.github.com>
2022-11-12 19:11:29 +0200

    Create configurator.sh
    Configurator de retea pentru UPB-Campus destinat echipamentelor care au instalat firmware-ul OpenWRT
    
    Instructiuni: Dupa ce ai instalat OpenWRT pe router-ul tau, conecteaza te la un hotspot WiFi din interfata de browser OpenWRT. Deschide un terminal sau CMD si introdu comanda: ssh root@192.168.1.1 Dupa ce te-ai logat in SSH-ul router-ului, foloseste urmatoarele comenzi pentru a rula script-ul:
    
    cd ~ | wget https://raw.githubusercontent.com/DariusIurca/UPBCampus-Config/main/configurator.sh | chmod +x configurator.sh | ./configurator.sh |
    
    Acum, urmeaza instructiunile configuratorului.

---
+++ configurator.sh
@@ -0,0 +1,39 @@
+
+ cd /etc/config/
+ cat > wpa.conf <<EOF
+ctrl_interface=/var/run/wpa_supplicant
+ap_scan=0
+network={
+        key_mgmt=IEEE8021X
+        eap=PEAP
+        identity="$mail"
+        password="$psw"
+        phase2="auth=MSCHAPV2"
+        priority=2
+}
+EOF
+ echo "Configurare router"
+ echo -e "Introduceti numele interfetei wan (vizibila in meniul OpenWRT): "
+ read interfata
+ cd /etc/init.d/
+ cat > wpa-autostart <<EOF
+#!/bin/sh /etc/rc.common
+# Copyright (C) 2007 OpenWrt.org
+START=99
+
+start() {
+echo start
+wpa_supplicant -D wired -i $interfata -c /etc/config/wpa.conf &
+}
+EOF
+ echo "Conectati cablul de internet in portul WAN al router-ului (daca acesta nu este conectat deja)"
+ echo -e "Pentru a continua apasati ENTER"
+ read con
+ ifdown $interfata
+ ifup $interfata
+ chmod +x /etc/init.d/wpa-autostart
+ /etc/init.d/wpa-autostart enable
+ /etc/init.d/wpa-autostart start
+ reboot
+ echo "Configurarea s-a finalizat! Spor la internet!" 
+fi
